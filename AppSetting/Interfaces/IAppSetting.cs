c597151205bf72b21ae6a5f3e482fd790d751405
Helder Marques <hasm.marques@gmail.com>
2021-02-16 08:43:48 +0000

    Added AppSetting project

---
+++ AppSetting/Interfaces/IAppSetting.cs
@@ -0,0 +1,21 @@
+﻿#region
+
+using System;
+
+#endregion
+
+namespace AppSetting.Interfaces
+{
+	public interface IAppSetting
+	{
+		#region Public Methods
+
+		bool GetBooleanFromKeyValue(string key);
+		DateTime GetDateTimeFromKeyValue(string key);
+		int GetIntegerFromKeyValue(string key);
+		string GetStringFromKeyValue(string key);
+		string GetConnectionString(string connectionName);
+
+		#endregion
+	}
+}
---
+++ AppSetting/Services/AppSetting.cs
@@ -0,0 +1,68 @@
+﻿#region
+
+using System;
+using AppSetting.Interfaces;
+using Microsoft.Extensions.Configuration;
+
+#endregion
+
+namespace AppSetting.Services
+{
+	public class AppSetting : IAppSetting
+	{
+		#region Public Methods
+
+		public string GetStringFromKeyValue(string key)
+		{
+			return GetConfig().GetSection(key).Value;
+		}
+
+		public bool GetBooleanFromKeyValue(string key)
+		{
+			var value = GetStringFromKeyValue(key);
+
+			if (!bool.TryParse(value, out var result))
+				throw new Exception($"Value of Application Setting '{key}' is not a boolean value: {value}");
+
+			return result;
+		}
+
+		public int GetIntegerFromKeyValue(string key)
+		{
+			var value = GetStringFromKeyValue(key);
+
+			if (!int.TryParse(value, out var result))
+				throw new Exception($"Value of Application Setting '{key}' is not a integer value: {value}");
+
+			return result;
+		}
+
+		public DateTime GetDateTimeFromKeyValue(string key)
+		{
+			var value = GetStringFromKeyValue(key);
+
+			if (!DateTime.TryParse(value, out var result))
+				throw new Exception($"Value of Application Setting '{key}' is not a DateTime value: {value}");
+
+			return result;
+		}
+
+		public string GetConnectionString(string connectionName)
+		{
+			return GetConfig()[connectionName];
+		}
+
+		#endregion
+
+		#region Private Methods
+
+		private IConfiguration GetConfig()
+		{
+			var builder = new ConfigurationBuilder().SetBasePath(AppContext.BaseDirectory)
+				.AddJsonFile("appsettings.json", true, true);
+			return builder.Build();
+		}
+
+		#endregion
+	}
+}
